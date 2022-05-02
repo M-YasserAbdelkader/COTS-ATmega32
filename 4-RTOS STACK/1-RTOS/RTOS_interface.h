c2c63574f7bf4ac672658ee94f3ddff50a00f114
M.YasserAbdelkader <M.YasserAbdelkaderm.abdelkader.yasser@gmail.com>
2022-05-02 02:17:21 +0200

    add prototypes and comments to the functions

--- 4-RTOS STACK/1-RTOS/RTOS_interface.h
+++ 4-RTOS STACK/1-RTOS/RTOS_interface.h
@@ -9,8 +9,37 @@
 #ifndef _RTOS_INTERFACE_H_
 #define _RTOS_INTERFACE_H_
 
+/****************************************************************************************************************
+ * \fn      void RTOS_voidStart(void);
+ * \brief   Starts RTOS
+ * \return  None
+ ****************************************************************************************************************/
 void RTOS_voidStart(void);
 
-void RTOS_voidCreateTask(u8 copy_u8Priority, u16 copy_u16Periodicity, void (*copy_pvTaskFunc)(void));
+u8 RTOS_u8CreateTask(u8 copy_u8Priority, u16 copy_u16Periodicity, void (*copy_pvTaskFunc)(void), u16 copy_u16FirstDelay);
+
+/****************************************************************************************************************
+ * \fn      void RTOS_voidSuspendTask(u8 copy_u8Priority);
+ * \brief   Suspend  Ready Task Given its priority
+ * \param   copy_u8Priority u8 Priority
+ * \return  None
+ ****************************************************************************************************************/
+void RTOS_voidSuspendTask(u8 copy_u8Priority);
+
+/****************************************************************************************************************
+ * \fn      void RTOS_voidResumeTask(u8 copy_u8Priority);
+ * \brief   Resume suspended Task Given its priority
+ * \param   copy_u8Priority u8 Priority
+ * \return  None
+ ****************************************************************************************************************/
+void RTOS_voidResumeTask(u8 copy_u8Priority);
+
+/****************************************************************************************************************
+ * \fn      void RTOS_voidDeleteTask(u8 copy_u8Priority);
+ * \brief   Delete Ready or Suspended Task Given its priority
+ * \param   copy_u8Priority u8 Priority
+ * \return  None
+ ****************************************************************************************************************/
+void RTOS_voidDeleteTask(u8 copy_u8Priority);
 
 #endif
