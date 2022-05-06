bc6f55f767caf9420290fb020b760c05f4e97e7c
M.YasserAbdelkader <M.YasserAbdelkaderm.abdelkader.yasser@gmail.com>
2022-05-06 22:24:26 +0200

    Include Error States

--- 1-MCAL/3-EXTI/EXTI_interface.h
+++ 1-MCAL/3-EXTI/EXTI_interface.h
@@ -2,7 +2,7 @@
  * @file        : EXTI_interface.h
  * @author      : Yasser Mustafa
  * @brief       : External interrupt (EXTI) Driver - MCAL
- * @version     : 0.1
+ * @version     : 0.2
  * @date        : 2022-04-18
  * @copyright   : Copyright (c) 2022
  *************************************************************/
@@ -11,44 +11,63 @@
 #define EXTI_REGISTER_H_
 
 /** \brief Sense Control for  INT0 */
-#define EXTI_INTO_LOW_LEVEL		    0
-#define EXTI_INTO_LOGIC_CHANGE	    1
-#define EXTI_INTO_FALLING_EDGE	    2
-#define EXTI_INTO_RISING_EDGE	    3
-
-/** \brief Sense Control for  INT1*/
-#define EXTI_INT1_LOW_LEVEL		    0
-#define EXTI_INT1_LOGIC_CHANGE	    1
-#define EXTI_INT1_FALLING_EDGE	    2
-#define EXTI_INT1_RISING_EDGE	    3
-
-
-/** \brief Sense Control for  INT2 */
-#define EXTI_INT2_FALLING_EDGE	    0
-#define EXTI_INT2_RISING_EDGE	    1
+#define EXTI_LOW_LEVEL		    0
+#define EXTI_LOGIC_CHANGE	    1
+#define EXTI_FALLING_EDGE	    2
+#define EXTI_RISING_EDGE	    3
 
 /** \brief EXTI 0, 1, 2 */
 #define EXTI_INT0			        0
 #define EXTI_INT1			        1
 #define EXTI_INT2			        2
 
-/*************************************************************
- *  \fn     void MEXTI_voidIntEnable(u8 copy_u8EXTI_NUM, u8 copy_u8EXTI_SENSE); 
- *  \brief  Enable External Interrupt
- *  \param  copy_u8EXTI_NUM EXTI_INT0 || EXTI_INT1 || EXTI_INT2
- *  \param  copy_u8EXTI_SENSE 
- *  \return None
-*************************************************************/
-void MEXTI_voidIntEnable(u8 copy_u8EXTI_NUM, u8 copy_u8EXTI_SENSE);
+/****************************************************************************************************************
+ *  \fn     u8 MEXTI_voidIntEnable(u8 copy_u8EXTI_NUM, u8 copy_u8EXTI_SENSE); 
+ *  \brief  Enable External interrupt and set its Sense control
+ *  \param  copy_u8EXTI_NUM External interrupt to be Enabled.
+ *          it takes the following values:
+ * \li \c   EXTI_INT0
+ * \li \c   EXTI_INT1
+ * \li \c   EXTI_INT2 
+ * 
+ * \param   copy_u8EXTI_SENSE Sense Control variable  
+ *          it takes the following values:
+ *          for EXTI_INT0:
+ * \li \c   EXTI_LOW_LEVEL
+ * \li \c   EXTI_LOGIC_CHANGE
+ * \li \c   EXTI_FALLING_EDGE
+ * \li \c   EXTI_RISING_EDGE
+ *          for EXTI_INT2:
+ * \li \c   EXTI_FALLING_EDGE
+ * \li \c   EXTI_RISING_EDGE
+ *          
+ * 
+ * \return  Upon Success, Function returns \c OK. OtherWise,
+ *          OtherWise, function returns one of the following error flags:
+ * \li \c   NOK                 if \c copy_u8EXTI_NUM is not a valid option 
+ *                              and/or \c copy_u8EXTI_SENSE     
+ *  
+ ****************************************************************************************************************/
+u8 MEXTI_voidIntEnable(u8 copy_u8EXTI_NUM, u8 copy_u8EXTI_SENSE);
 
-/*************************************************************
- *  \fn     void MEXTI_voidIntDisable(u8 copy_u8EXTI_NUM); 
- *  \brief  Disable External Interrupt
- *  \param  copy_u8EXTI_NUM EXTI_INT0 || EXTI_INT1 || EXTI_INT2 
- *  \return None
-*************************************************************/
-void MEXTI_voidIntDisable(u8 copy_u8EXTI_NUM);
-void MEXTI_voidSetCallBack(void (*copy_ptrToFunction)(void), u8 copy_u8Index);
+/****************************************************************************************************************
+ *  \fn     u8 MEXTI_voidIntDisable(u8 copy_u8EXTI_NUM); 
+ *  \brief  Disable External interrupt
+ *  \param  copy_u8EXTI_NUM External interrupt to be disabled.
+ *          it takes the following values:
+ * \li \c   EXTI_INT0
+ * \li \c   EXTI_INT1
+ * \li \c   EXTI_INT2 
+ * 
+ * \return  Upon Success, Function returns \c OK. OtherWise,
+ *          OtherWise, function returns one of the following error flags:
+ * \li \c   NOK                 if \c copy_u8EXTI_NUM is not a valid option   
+ *  
+ ****************************************************************************************************************/
+u8 MEXTI_voidIntDisable(u8 copy_u8EXTI_NUM);
+
+
+u8 MEXTI_voidSetCallBack(void (*copy_ptrToFunction)(void), u8 copy_u8Index);
 
 
 #endif
--- 1-MCAL/3-EXTI/EXTRI_program.c
+++ 1-MCAL/3-EXTI/EXTRI_program.c
@@ -2,7 +2,7 @@
  * @file    	: EXTI_program.c
  * @author  	: Yasser Mustafa
  * @brief   	: External interrupt (EXTI) Driver - MCAL
- * @version 	: 0.1
+ * @version 	: 0.2
  * @date 		: 2022-04-18
  * @copyright 	: Copyright (c) 2022
  *************************************************************/
@@ -15,149 +15,173 @@
 #include "EXTRI_register.h"
 #include "EXTI_config.h"
 
+static void (*pf[3])(void) = {NULL, NULL, NULL};
 
-static void (*pf[3])(void)= {NULL, NULL, NULL};
-
-void MEXTI_voidIntEnable(u8 copy_u8EXTI_NUM, u8 copy_u8EXTI_SENSE)
+u8 MEXTI_voidIntEnable(u8 copy_u8EXTI_NUM, u8 copy_u8EXTI_SENSE)
 {
 
-	/** \brief Check which external interrupt is to be enabled*/
-	switch (copy_u8EXTI_NUM)
+	u8 local_u8ErrorState = OK;
+	/* Validate Input */
+	if (copy_u8EXTI_NUM > EXTI_INT2)
 	{
-			case EXTI_INT0:
-					SET_BIT(GICR, GICR_INT_0);
-							/** \brief Check which sense mode is to be activated */
-							switch(copy_u8EXTI_SENSE)
-							{
-										case EXTI_INTO_LOW_LEVEL:
-																	CLR_BIT(MCUCR, MCUCR_ISC00);
-																	CLR_BIT(MCUCR, MCUCR_ISC01);
-
-																	break;
-
-										case EXTI_INTO_LOGIC_CHANGE:
-																	SET_BIT(MCUCR, MCUCR_ISC00);
-																	CLR_BIT(MCUCR, MCUCR_ISC01);
-
-																	break;
-
-										case EXTI_INTO_FALLING_EDGE:
-																	CLR_BIT(MCUCR, MCUCR_ISC00);
-																	SET_BIT(MCUCR, MCUCR_ISC01);
-
-																	break;
-
-										case EXTI_INTO_RISING_EDGE:
-																	SET_BIT(MCUCR, MCUCR_ISC00);
-																	SET_BIT(MCUCR, MCUCR_ISC01);
-
-																	break;
-							}
-
-			break;
-
-
-			case EXTI_INT1:
-							switch(copy_u8EXTI_SENSE)
-							{
-										case EXTI_INT1_LOW_LEVEL:
-																	CLR_BIT(MCUCR, MCUCR_ISC10);
-																	CLR_BIT(MCUCR, MCUCR_ISC11);
-																	SET_BIT(GICR, GICR_INT_1);
-																	break;
-										case EXTI_INT1_LOGIC_CHANGE:
-																	SET_BIT(MCUCR, MCUCR_ISC10);
-																	CLR_BIT(MCUCR, MCUCR_ISC11);
-																	SET_BIT(GICR, GICR_INT_1);
-																	break;
-										case EXTI_INT1_FALLING_EDGE:
-																	CLR_BIT(MCUCR, MCUCR_ISC10);
-																	SET_BIT(MCUCR, MCUCR_ISC11);
-																	SET_BIT(GICR, GICR_INT_1);
-																	break;
-										case EXTI_INT1_RISING_EDGE:
-																	SET_BIT(MCUCR, MCUCR_ISC10);
-																	SET_BIT(MCUCR, MCUCR_ISC11);
-																	SET_BIT(GICR, GICR_INT_1);
-																	break;
-										}
-			break;
-
-			case EXTI_INT2:
-										switch(copy_u8EXTI_SENSE)
-										{
-
-										case EXTI_INT2_FALLING_EDGE:
-																	CLR_BIT(MCUCSR, MCUCSR_INT2);
-																	SET_BIT(GICR, GICR_INT_2);
-																	break;
-										case EXTI_INT2_RISING_EDGE:
-																	SET_BIT(MCUCSR, MCUCSR_INT2);
-																	SET_BIT(GICR, GICR_INT_2);
-																	break;
-
-										}
-			break;
-
+		local_u8ErrorState = NOK;
 	}
+	else if ((copy_u8EXTI_NUM <= EXTI_INT1) && (copy_u8EXTI_SENSE > EXTI_RISING_EDGE))
+	{
+		local_u8ErrorState = NOK;
+	}
+	else if ((copy_u8EXTI_NUM == EXTI_INT2) && ((copy_u8EXTI_SENSE > EXTI_RISING_EDGE) || copy_u8EXTI_SENSE < EXTI_FALLING_EDGE))
+	{
+		local_u8ErrorState = NOK;
+	}
+	else
+	{
+		/** \brief Check which external interrupt is to be enabled*/
+		switch (copy_u8EXTI_NUM)
+		{
+		case EXTI_INT0:
+			SET_BIT(GICR, GICR_INT_0);
+			/** \brief Check which sense mode is to be activated */
+			switch (copy_u8EXTI_SENSE)
+			{
+			case EXTI_LOW_LEVEL:
+				CLR_BIT(MCUCR, MCUCR_ISC00);
+				CLR_BIT(MCUCR, MCUCR_ISC01);
+				break;
+
+			case EXTI_LOGIC_CHANGE:
+				SET_BIT(MCUCR, MCUCR_ISC00);
+				CLR_BIT(MCUCR, MCUCR_ISC01);
+				break;
+
+			case EXTI_FALLING_EDGE:
+				CLR_BIT(MCUCR, MCUCR_ISC00);
+				SET_BIT(MCUCR, MCUCR_ISC01);
+				break;
+
+			case EXTI_RISING_EDGE:
+				SET_BIT(MCUCR, MCUCR_ISC00);
+				SET_BIT(MCUCR, MCUCR_ISC01);
+				break;
+			}
+			break;
+
+		case EXTI_INT1:
+			switch (copy_u8EXTI_SENSE)
+			{
+			case EXTI_LOW_LEVEL:
+				CLR_BIT(MCUCR, MCUCR_ISC10);
+				CLR_BIT(MCUCR, MCUCR_ISC11);
+				SET_BIT(GICR, GICR_INT_1);
+				break;
+
+			case EXTI_LOGIC_CHANGE:
+				SET_BIT(MCUCR, MCUCR_ISC10);
+				CLR_BIT(MCUCR, MCUCR_ISC11);
+				SET_BIT(GICR, GICR_INT_1);
+				break;
+
+			case EXTI_FALLING_EDGE:
+				CLR_BIT(MCUCR, MCUCR_ISC10);
+				SET_BIT(MCUCR, MCUCR_ISC11);
+				SET_BIT(GICR, GICR_INT_1);
+				break;
+
+			case EXTI_RISING_EDGE:
+				SET_BIT(MCUCR, MCUCR_ISC10);
+				SET_BIT(MCUCR, MCUCR_ISC11);
+				SET_BIT(GICR, GICR_INT_1);
+				break;
+			}
+			break;
+
+		case EXTI_INT2:
+			switch (copy_u8EXTI_SENSE)
+			{
+			case EXTI_FALLING_EDGE:
+				CLR_BIT(MCUCSR, MCUCSR_INT2);
+				SET_BIT(GICR, GICR_INT_2);
+				break;
+
+			case EXTI_RISING_EDGE:
+				SET_BIT(MCUCSR, MCUCSR_INT2);
+				SET_BIT(GICR, GICR_INT_2);
+				break;
+			}
+			break;
+		}
+	}
+	return local_u8ErrorState;
 }
 
-
-
-void MEXTI_voidIntDisable(u8 copy_u8EXTI_NUM)
+u8 MEXTI_voidIntDisable(u8 copy_u8EXTI_NUM)
 {
-	switch(copy_u8EXTI_NUM)
+	u8 local_u8ErrorState = OK;
+	if (copy_u8EXTI_NUM > EXTI_INT2)
 	{
-							case EXTI_INT0:
-											CLR_BIT(GICR, GIFR_INT_0);
-											break;
-							case EXTI_INT1:
-											CLR_BIT(GICR, GIFR_INT_1);
-											break;
-							case EXTI_INT2:
-											CLR_BIT(GICR, GIFR_INT_2);
-											break;
-
+		local_u8ErrorState = NOK;
 	}
+	else
+	{
+		switch (copy_u8EXTI_NUM)
+		{
+		case EXTI_INT0:
+			CLR_BIT(GICR, GIFR_INT_0);
+			break;
+		case EXTI_INT1:
+			CLR_BIT(GICR, GIFR_INT_1);
+			break;
+		case EXTI_INT2:
+			CLR_BIT(GICR, GIFR_INT_2);
+			break;
+		}
+	}
+	return local_u8ErrorState;
 }
 
-
-
-void __vector_1(void)		__attribute__((signal));
+void __vector_1(void) __attribute__((signal));
 void __vector_1(void)
 {
-	if(pf[EXTI_INT0] != NULL)
+	if (pf[EXTI_INT0] != NULL)
 	{
 		pf[EXTI_INT0]();
 	}
-
 }
 
-void __vector_2(void)		__attribute__((signal));
+void __vector_2(void) __attribute__((signal));
 void __vector_2(void)
 {
-	if(pf[EXTI_INT1] != NULL)
+	if (pf[EXTI_INT1] != NULL)
 	{
 		pf[EXTI_INT1]();
 	}
 }
 
-void __vector_3(void)		__attribute__((signal));
+void __vector_3(void) __attribute__((signal));
 void __vector_3(void)
 {
-	if(pf[EXTI_INT2] != NULL)
+	if (pf[EXTI_INT2] != NULL)
 	{
 		pf[EXTI_INT2]();
 	}
 }
 
-
-void MEXTI_voidSetCallBack(void (*copy_ptrToFunction)(void), u8 copy_u8Index)
+u8 MEXTI_voidSetCallBack(void (*copy_ptrToFunction)(void), u8 copy_u8Index)
 {
-	pf[copy_u8Index] = copy_ptrToFunction;
-
+	u8 local_u8ErrorState = OK;
+	
+	/* Check input Validity */
+	if (copy_ptrToFunction == NULL)
+	{
+		local_u8ErrorState = NULL_POINTER;
+	}
+	else if (copy_u8Index > EXTI_INT2)
+	{
+		local_u8ErrorState = NOK;
+	}
+	else
+	{
+		pf[copy_u8Index] = copy_ptrToFunction;
+	}
+	return local_u8ErrorState;
 }
-
-
-
-
